5ce5dc8ed6491369bebf2c7d759b61262c2be351
lisa-analytics <lisakulpanova.work@gmail.com>
2026-02-04 23:23:22 +0100

    Update 06_purchase_statistics_by_channel.sql

--- sql/06_purchase_statistics_by_channel.sql
+++ sql/06_purchase_statistics_by_channel.sql
@@ -1,3 +1,6 @@
+-- Purpose: Compare online and offline sales by total quantity and number of unique orders
+-- Output: purchase_type, total_quantity, total_unique_orders
+
 select 'online' as purchase_type, SUM(quantity) as total_quantity,
 COUNT(distinct order_id) as total_unique_orders
 from order_items_sql_project 
