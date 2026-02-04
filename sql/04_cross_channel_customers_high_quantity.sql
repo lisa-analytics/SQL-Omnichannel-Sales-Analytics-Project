bf0f9ac5e61718ddbfedb52404cbe5a843733af8
lisa-analytics <lisakulpanova.work@gmail.com>
2026-02-04 23:22:19 +0100

    Update 04_cross_channel_customers_high_quantity.sql

--- sql/04_cross_channel_customers_high_quantity.sql
+++ sql/04_cross_channel_customers_high_quantity.sql
@@ -1,9 +1,15 @@
-select osp.user_id 
+-- Purpose: Find users who purchased more than two units of the same product in both online and offline channels
+-- Output: user_id
+
+select distinct osp.user_id 
 from orders_sql_project osp 
 join order_items_sql_project oisp on osp.order_id = oisp.order_id
-where oisp.quantity > 2
-and osp.user_id IN(
-select so.user_id
+where osp.user_id is not null
+and oisp.quantity > 2
+intersect 
+select distinct so.user_id
 from store_orders so 
-join store_order_items soi on so.store_order_id = soi.store_order_id )
-order by osp.user_id asc;
+join store_order_items soi on so.store_order_id = soi.store_order_id
+where so.user_id is not null
+and soi.quantity > 2
+order by user_id asc;
