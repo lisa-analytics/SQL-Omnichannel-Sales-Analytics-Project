0208be5c85a65f0e1a8d2fa10b59a92b4ebc7a31
lisa-analytics <lisakulpanova.work@gmail.com>
2026-02-04 23:21:34 +0100

    Update 02_all_orders_online_offline.sql

--- sql/02_all_orders_online_offline.sql
+++ sql/02_all_orders_online_offline.sql
@@ -3,7 +3,9 @@
 
 select user_id, order_date, order_id as order_id
 from orders_sql_project 
+where user_id is not null
 union all 
 select user_id, order_date, store_order_id as order_id
 from store_orders
+where user_id is not null
 order by user_id, order_date, order_id;
