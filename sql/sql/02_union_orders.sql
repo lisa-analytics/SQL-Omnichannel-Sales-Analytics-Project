ed2d841b106b9e2d91c892ed87e81d933c625763
lisa-analytics <lisakulpanova.work@gmail.com>
2026-02-04 21:36:44 +0100

    Delete sql/sql/02_union_orders.sql

--- sql/sql/02_union_orders.sql
+++
@@ -1,6 +0,0 @@
-select user_id, order_date, order_id as order_id
-from orders_sql_project 
-union all 
-select user_id, order_date, store_order_id as order_id
-from store_orders
-order by user_id, order_date, order_id;
