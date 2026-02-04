c30821fa38758cf7308bd9dce8867b92bb33a7ca
lisa-analytics <lisakulpanova.work@gmail.com>
2026-02-04 21:23:58 +0100

    Create 01_total_spend_by_user.sql

---
+++ sql/01_total_spend_by_user.sql
@@ -0,0 +1,7 @@
+select osp.user_id, 
+SUM (oisp.quantity * psp.product_price) as total_spent
+from orders_sql_project osp
+join order_items_sql_project oisp on osp.order_id = oisp.order_id
+join products_sql_project psp on oisp.product_id = psp.product_id
+group by osp.user_id 
+order by total_spent desc;
